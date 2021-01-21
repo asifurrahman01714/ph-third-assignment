fe1b052f18294c74b878944ead375a8a73357fbf
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-01-21 23:43:12 +0600

    fixed the indentation error

--- assignment.js
+++ assignment.js
@@ -2,63 +2,78 @@
 
 // 1.convert Kilometer to meter
 
-function kilometerToMeter(kilometer){
-    var meter = kilometer* 1000;
-    return  Math.abs(meter);//Math.abs() function will work for converting negative number to positive number.
+function kilometerToMeter(kilometer) {
+    if (kilometer < 0) {
+        return "Plese input a positive value"; //Detecting negative value of kilometer as it is invalid.
+    } else {
+        var meter = kilometer * 1000;
+        return meter;
+    }
 }
-var result = kilometerToMeter(-12);
+var result = kilometerToMeter(12);
 console.log(result, 'meter');
 
 
 //2.Calculate Budget to buy watch,phone & laptop.
-function budgetCalculator(watch, phone, laptop){
-    var watch = watch * 50;
-    var phone = phone * 100;
-    var laptop = laptop * 500;
-    var sum = watch + phone + laptop;
-    return  sum;
+function budgetCalculator(watch, phone, laptop) {
+    if (watch < 0 || phone < 0 || laptop < 0) {
+        return "Plese input positive value";//Detecting negative value of the things as it is invalid.
+    } else {
+        var watch = watch * 50;
+        var phone = phone * 100;
+        var laptop = laptop * 500;
+        var sum = watch + phone + laptop;
+        return sum;
+    }
 }
-var result = budgetCalculator(0, 2, 3);
-console.log('you need ', result, 'dollar to buy these item');
+var result = budgetCalculator(-1, 2, 3);
+console.log(result);
 
 
 
 //3.Calculate Hotel cost.
 
 function hotelCost(day) {
-    if (day<=10) {
-        var totalCost = day *100;
-    } else if(day<=20) {
-        var firstPart = 10*100;
-        var remainingPart = day -10;
-        var secondPart= remainingPart *80;
-        totalCost = firstPart + secondPart;
+    if (day < 0) {
+        return "Plese input positive value as day";//Detecting negative value as it is invalid.
+    } else {
+        if (day <= 10) {
+            var totalCost = day * 100;
+        } else if (day <= 20) {
+            var firstPart = 10 * 100;
+            var remainingPart = day - 10;
+            var secondPart = remainingPart * 80;
+            totalCost = firstPart + secondPart;
+        } else {
+            var firstPart = 10 * 100;
+            var secondPart = 10 * 80;
+            var remainingPart = day - 20;
+            var thirdPart = remainingPart * 50;
+            totalCost = firstPart + secondPart + thirdPart;
+        }
+        return totalCost;
     }
-    else{
-        var firstPart = 10*100;
-        var secondPart = 10*80;
-        var remainingPart = day - 20;
-        var thirdPart = remainingPart * 50;
-        totalCost= firstPart+ secondPart+ thirdPart;
-    }
-    return totalCost;
 }
-var result = hotelCost(21);
-console.log("Your total hotel cost is",result,"taka");
+var result = hotelCost(-21);
+console.log(result);
 
 
 
 //4.Find the biggest friend Name.
 
 function megaFriend(name) {
-    var megaName = name[0];
-    for (var i = 0; i < name.length; i++) {
-        var currentName = name[i];
-        if (currentName.length > megaName.length) {
-            megaName = currentName;
+    if (name.length == 0) {
+        return "please input a valid array"// Detecting empty array
+    } else {
+        var megaName = name[0];
+        for (var i = 0; i < name.length; i++) {
+            var currentName = name[i];
+            if (currentName.length > megaName.length) {
+                megaName = currentName;
+            }
         }
+        return megaName;
     }
-    return megaName;
 }
-var result = megaFriend(['asif', 'sabbir ahmed', 'sakibal hasan', 'mashrafi', 'abid']);
-console.log('The mega name is: ', result);
+var result = megaFriend(["asif", "sakib", "mashrafi"]);
+console.log(result);
