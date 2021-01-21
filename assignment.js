6d3bb5ffc8edd18abf81a7f209645268dc53aaf7
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-01-21 23:56:27 +0600

    All error fixed

--- assignment.js
+++ assignment.js
@@ -4,21 +4,21 @@
 
 function kilometerToMeter(kilometer) {
     if (kilometer < 0) {
-        return "Plese input a positive value"; //Detecting negative value of kilometer as it is invalid.
+        return "distance cannot be negative"; //Detecting negative value of kilometer as it is invalid.
     } else {
         var meter = kilometer * 1000;
         return meter;
     }
 }
-var result = kilometerToMeter(12);
-console.log(result, 'meter');
+var result = kilometerToMeter(1.5);
+console.log(result,);
 
 
 //2.Calculate Budget to buy watch,phone & laptop.
 
 function budgetCalculator(watch, phone, laptop) {
     if (watch < 0 || phone < 0 || laptop < 0) {
-        return "Plese input positive value";//Detecting negative value of the things as it is invalid.
+        return "Things cannot be negative";//Detecting negative value of the things as it is invalid.
     } else {
         var watch = watch * 50;
         var phone = phone * 100;
@@ -27,7 +27,7 @@ function budgetCalculator(watch, phone, laptop) {
         return sum;
     }
 }
-var result = budgetCalculator(-1, 2, 3);
+var result = budgetCalculator(1, 2, 3);
 console.log(result);
 
 
@@ -36,7 +36,7 @@ console.log(result);
 
 function hotelCost(day) {
     if (day < 0) {
-        return "Plese input positive value as day";//Detecting negative value as it is invalid.
+        return "Day cannot be a negative number";//Detecting negative value as it is invalid.
     } else {
         if (day <= 10) {
             var totalCost = day * 100;
@@ -55,7 +55,7 @@ function hotelCost(day) {
         return totalCost;
     }
 }
-var result = hotelCost(-21);
+var result = hotelCost(21);
 console.log(result);
 
 
@@ -64,7 +64,7 @@ console.log(result);
 
 function megaFriend(name) {
     if (name.length == 0) {
-        return "please input a valid array"// Detecting empty array
+        return "Array cannot be an empty array"// Detecting empty array
     } else {
         var megaName = name[0];
         for (var i = 0; i < name.length; i++) {
